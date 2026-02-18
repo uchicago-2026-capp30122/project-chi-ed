fdc1042186c17aab2302dd36d342d7d3ff375aa3
Apollinaire <eabi@uchicago.edu>
2026-02-17 20:30:55 -0600

    updated the repo structure

---
+++ code/hacking/s01_load.py
@@ -0,0 +1,6 @@
+import pathlib
+import pandas
+import csv
+
+
+INPUT_DIRPATH = pathlib.Path(__file__).parent.parent.parent.resolve() / "data" / "raw"
---
+++ code/src/dataloader.py
@@ -0,0 +1,1 @@
+"""Helper functions to HTTP request or load data, or merge data"""
