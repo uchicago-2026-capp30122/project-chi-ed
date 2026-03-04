870e15557b852c60a01a91206187330944ee0e9f
Faizan <imranfaiz@uchicago.edu>
2026-03-04 12:58:53 -0600

    Merged the report card df with api df and wrote the combined df inside the merged_data folder

--- chi_ed/api_rc_merge/merge_api_rc.py
+++ chi_ed/api_rc_merge/merge_api_rc.py
@@ -10,35 +10,10 @@ from pathlib import Path
 # ------------------------------------------------------------------------------
 
 MERGE_CSV_PATH = (
-    Path(__file__).parent.parent.parent / "data/outputs/maerged_data/merged_api_rc.csv"
+    Path(__file__).parent.parent.parent / "data/outputs/merged_data/merged_api_rc.csv"
 )
 
-
-def merge_api_rc(output_filname: Path = MERGE_CSV_PATH):
-    """
-    This function takes in a Path object and inside the function we are importing
-    different data sources, performing a fuzzy match and writing the merged data
-    as a csv file.
-
-    Inputs:
-        output_filename: Path object for where we write the csv merged data
-    """
-    report_card_df = merge_for_zip(DIRECTORY_DATA_PATH, REPORT_CARD_PATH)
-    api_df = clean_api_json(RAW_DATA_API)
-
-    api_df = api_df.rename({"school_long_name"})
-
-    api_school_zip = pl.Series(
-        api_df.select(pl.concat_list("school_short_name", "zip"))
-    ).to_list()
-    rc_school_zip = pl.Series(
-        report_card_df.select(pl.concat_list("school_name", "zip"))
-    ).to_list()
-
-    # Converting list of list to dict for faster lookups
-
-    api_dict = {key: value for key, value in api_school_zip}
-    rc_dict = {key: value for key, value in rc_school_zip}
+def matched_schools(api_dict, rc_dict):
 
     school_similarity_lookup = dict()
 
@@ -67,20 +42,65 @@ def merge_api_rc(output_filname: Path = MERGE_CSV_PATH):
                 > 0.71 # Only one school is being wrongly matched 
             ):
                 school_similarity_lookup[school_x].append(
-                    (
-                        matched_school,
-                        jellyfish.jaro_winkler_similarity(
-                            clean_school_x, clean_matched_school
-                        ),
-                    )
+                    (matched_school, jellyfish.jaro_winkler_similarity(clean_school_x, clean_matched_school))
                 )
 
-    matched = 0
+    school_similarity_lookup["EXCEL ENGLEWOOD HS"] = [] # this school was wrongly matched
 
-    for _, v in school_similarity_lookup.items():
+    for k, v in school_similarity_lookup.items():
         v = sorted(v, key=lambda tup: tup[1], reverse=True)
         if v:
-            matched += 1
             v = v[0]
+            school_similarity_lookup.update({k:v[0]})
+        else:
+            school_similarity_lookup.update({k:""})
+
+    return school_similarity_lookup
+
+
+def merge_api_rc(output_filname: Path = MERGE_CSV_PATH):
+    """
+    This function takes in a Path object and inside the function we are importing
+    different data sources, performing a fuzzy match and writing the merged data
+    as a csv file.
+
+    Inputs:
+        output_filename: Path object for where we write the csv merged data
+    """
+    report_card_df = merge_for_zip(DIRECTORY_DATA_PATH, REPORT_CARD_PATH)
+    api_df = clean_api_json(RAW_DATA_API)
+
+    api_school_zip = pl.Series(
+        api_df.select(pl.concat_list("school_short_name", "zip"))
+    ).to_list()
+    rc_school_zip = pl.Series(
+        report_card_df.select(pl.concat_list("school_name", "zip"))
+    ).to_list()
+
+    # Converting list of list to dict for faster lookups
+
+    api_dict = {key: value for key, value in api_school_zip}
+    rc_dict = {key: value for key, value in rc_school_zip}
+
+    school_lookup = matched_schools(api_dict, rc_dict)
+
+    mapper = pl.DataFrame({
+    "school_short_name": list(school_lookup.keys()),
+    "school_name": list(school_lookup.values())
+    })
+
+    api_df = api_df.join(
+        mapper,
+        on="school_short_name",
+        how="inner"
+    )
+
+    merged_df = report_card_df.join(
+        api_df,
+        on="school_name",
+        how="full"
+    )
+
+    merged_df.write_csv(output_filname)
 
     
