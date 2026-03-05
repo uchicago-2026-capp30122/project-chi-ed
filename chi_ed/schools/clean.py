1c896fa5600c3462625023ae24e12268af155ea5
Apollinaire <eabi@uchicago.edu>
2026-03-05 17:50:46 -0600

    dowloaded 2024 reports cards

--- chi_ed/schools/clean.py
+++ chi_ed/schools/clean.py
@@ -12,7 +12,8 @@ DATA_DIRPATH = pathlib.Path(__file__).parent.parent.parent.resolve() / "data"
 OUTPUTS_DIRPATH = pathlib.Path(__file__).parent.parent.parent.resolve() / "outputs" 
 
 def import_data(filepath: pathlib.Path = None, raw: bool = True):
-    """Import raw or cleaned version of the data"""
+    """Import raw or cleaned version of the data. 
+    N0TE: For raw data this function is designed for report carrds"""
     if raw:
         data = load_reports_card(DATA_DIRPATH / "raw" / "report_card_data" / "2025 ISBE Reports Card.xlsx")
     else:
@@ -23,16 +24,19 @@ def import_data(filepath: pathlib.Path = None, raw: bool = True):
     return data
 
 
-# TASK 1: Clean the 2025 ISBE Reports Card data if it is not already cleaned
-# NOTE: Export the cleaned version if first importing this data locally
-cards_2025_dt = import_data(filepath = DATA_DIRPATH / "clean" / "report_cards_2025.csv", raw = False)
+def clean_reports_data():
+    """Clean report cards and merge with API data"""
+    # TODO: Complete this function to export merged and cleaned data for reports 
+    # (1) Clean the 2025 ISBE Reports Card data if it is not already cleaned
+    # NOTE: Export the cleaned version if first importing this data locally
+    cards_2025_dt = import_data(filepath = DATA_DIRPATH / "clean" / "report_cards_2025.csv", raw = False)
 
-# TASK 2: Import the merged report cards and API data 
-# NOTE: I iwll use import_data() even though that function seems designed for report card data. It is flexible on non-raw data
-merged_dt = import_data(filepath = DATA_DIRPATH / "outputs" / "merged_data" / "merged_api_rc.csv", raw = False)
+    # (2) Import the merged report cards and API data 
+    # NOTE: I iwll use import_data() even though that function seems designed for report card data. It is flexible on non-raw data
+    merged_dt = import_data(filepath = DATA_DIRPATH / "outputs" / "merged_data" / "merged_api_rc.csv", raw = False)
 
-# Save all columns names
-with open(OUTPUTS_DIRPATH / "explore" / "columns_merged_api_rc.txt", "w") as f:
-    for col in merged_dt.columns:
-        f.write(col + "\n")
+    # Save all columns names
+    with open(OUTPUTS_DIRPATH / "explore" / "columns_merged_api_rc.txt", "w") as f:
+        for col in merged_dt.columns:
+            f.write(col + "\n")
 
