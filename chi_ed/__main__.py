c7e8e347265f44402a8e5fe865eb4ee34cd7cfaf
Mehwish Waheed <mehwishwaheed@uchicago.edu>
2026-03-07 22:32:40 -0600

    random stuff for now

--- chi_ed/__main__.py
+++ chi_ed/__main__.py
@@ -1,7 +1,16 @@
 # Import all other modules in this file and run everything here
+import sys 
 from .spatial import dashboard
 from .reports.reports import create_report
 
 
 if __name__ == "__main__":
-    pass
+    num_args = len(sys.argv)
+
+    if num_args < 2:
+        raise ValueError("Brother what should I do?")
+    
+    task = str(sys.argv[1])
+
+    if task == "dashboard":
+        dashboard
