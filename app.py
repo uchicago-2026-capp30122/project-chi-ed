bf51bb129cf3ec4d3739f610893bc98015af4b05
Mehwish Waheed <mehwishwaheed@uchicago.edu>
2026-03-24 20:11:20 -0500

    rename wsgi to app for render

---
+++ app.py
@@ -0,0 +1,4 @@
+from chi_ed.spatial.dashboard import server as app
+
+if __name__ == "__main__":
+    app.run()
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-from chi_ed.spatial.dashboard import server
-
-if __name__ == "__main__":
-    server.run()
